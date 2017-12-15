49ec1aebba6d4ea41d0ab43ea9085274c880118a
prasangafdz@outlook.com <LykanHypersp0rt@#>
2017-12-15 17:08:35 +0530

    Fixed filename issues with the Models

--- app/UserDetails.php
+++ app/UserDetails.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class User_Details extends Model
+class UserDetails extends Model
 {
     //
 }
--- app/UserRole.php
+++ app/UserRole.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Student_Details extends Model
+class UserRole extends Model
 {
     //
 }
