ed7b38af6bdb7419bc59a7dbdb7e949d810d0ab9
prasangafdz@outlook.com <LykanHypersp0rt@#>
2017-12-27 18:32:53 +0530

    Added the sidebar

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts/app')<!--Extend from the app layout-->
 @section('content')
-<div class="container">
+<div class="">
+<div class="container col-lg-10 col-md-10 pull-left">
       <div class="header clearfix">
         <nav>
           <ul class="nav nav-pills pull-right">
@@ -51,10 +52,44 @@
     </div>
 </div>
 
-    <hr/>    
+    </div>
+</div>
+
+<div class ="col-lg-2 col-md-2 pull-right">
+          <div class="sidebar-module sidebar-module-inset">
+            <h4>About</h4>
+            <p style="text-align:center; font-size:14px">This is the web interface to Edit, Update and/or Delete records of the  <em>ABC International school Student Management System</em> </p>
+          </div>
+          <div class="sidebar-module">
+            <h4>Archives</h4>
+            <ol class="list-unstyled">
+              <li><a href="#">March 2014</a></li>
+              <li><a href="#">February 2014</a></li>
+              <li><a href="#">January 2014</a></li>
+              <li><a href="#">December 2013</a></li>
+              <li><a href="#">November 2013</a></li>
+              <li><a href="#">October 2013</a></li>
+              <li><a href="#">September 2013</a></li>
+              <li><a href="#">August 2013</a></li>
+              <li><a href="#">July 2013</a></li>
+              <li><a href="#">June 2013</a></li>
+              <li><a href="#">May 2013</a></li>
+              <li><a href="#">April 2013</a></li>
+            </ol>
+          </div>
+          <div class="sidebar-module">
+            <h4>Elsewhere</h4>
+            <ol class="list-unstyled">
+              <li><a href="#">GitHub</a></li>
+              <li><a href="#">Twitter</a></li>
+              <li><a href="#">Facebook</a></li>
+            </ol>
+          </div>
+</div>
+
+<hr/>    
       <footer class="footer">
         <p>© 2016 Company, Inc.</p>
       </footer>
 
-    </div>
     @endsection()
